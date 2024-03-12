a06c71a83212d748225c01a8124e540054bd0b62
tdinapoli <tomas.dina98@gmail.com>
2024-03-12 16:01:31 -0300

    fix: specify state and pin parameters at RPDI and RPDO initialization in config.py

--- src/refurbishedPTI/configs.py
+++ src/refurbishedPTI/configs.py
@@ -2,15 +2,15 @@
 import redpipy as rpp
 
 EMISSION_MONO_DRIVER = {
-'pin_step'  :  rpp.digital.RPDO((True, 'p', 4)),
-'direction'  :  rpp.digital.RPDO((True, 'p', 5)),
-'limit_switch'  :  rpp.digital.RPDO((True, 'p', 3)),
+'pin_step'  :  rpp.digital.RPDO(state=True, pin=('p', 4)),
+'direction'  :  rpp.digital.RPDO(state=True, pin=('p', 5)),
+'limit_switch'  :  rpp.digital.RPDO(state=True, pin=('p', 3)),
         }
 
 EXCITATION_MONO_DRIVER = {
-'pin_step'  :  rpp.digital.RPDO((True, 'p', 6)),
-'direction'  :  rpp.digital.RPDO((True, 'p', 7)),
-'limit_switch'  :  rpp.digital.RPDO((True, 'p', 2)),
+'pin_step'  :  rpp.digital.RPDO(state=True, pin=('p', 6)),
+'direction'  :  rpp.digital.RPDO(state=True, pin=('p', 7)),
+'limit_switch'  :  rpp.digital.RPDO(state=True, pin=('p', 2)),
         }
 
 PEAK_THRESHOLD = -3.5
